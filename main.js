22bce0532f79edac6cf828592a939cfd640fd5a6
camipro09 <137501612+camipro09@users.noreply.github.com>
2023-08-11 08:30:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log ('la suma es igual a', 2+2)
+console.log ('la resta es igual a', 2-2)
+console.log ('la multiplicacion es igual a', 2*4)
+console.log ('la division es igual a', 2/4)
+//punto2
+let sumarVariable = 4
+let restarVariable = 0
+let multiplicarVariable = 8
+let dividirVariable = 2
+//punto3
+console.log("variable suma," ,suma "variable resta" ,resta "variable multiplicacon" , multiplicacon "variable divison" ,division)
+alert("resultado de la suma es 4")
+alert("resultado de la resta es 0")
+alert("resultado de la multiplicacion es 8")
+alert("resultado de la division es 2")
+//punto4
+alert("mi edad es"2023-2009)
+//punto5
+prompt("ingrese su año de nacimiento")
+//punto6
+let nombre = prompt("ingrese su nombre")
+alert("bienvenido" + nombre)
+//punto7
+let nombre = prompt ("ingrese su nombre")
+let apellido = prompt("ingrese su apellido")
+alert("bienvenido" + nombre + apellido)
+//punto8
+let diez=10
+let tres=3
+let resultadodemultiplicacion = 10*3
